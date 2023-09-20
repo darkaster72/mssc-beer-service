9824dd47563a70803833735daf924647e43f0ffd
Ankit Sharma <sankit72@gmail.com>
2023-09-20 23:08:52 +0530

    first commit

---
+++ src/main/java/io/github/darkaster72/msscbrewery/MsscBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.darkaster72.msscbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/darkaster72/msscbrewery/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.darkaster72.msscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
