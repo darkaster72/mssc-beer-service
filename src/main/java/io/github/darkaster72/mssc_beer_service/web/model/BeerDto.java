d87250f75903b9e4937bf77d37d8b9205e2b77ac
Ankit Sharma <sankit72@gmail.com>
2023-09-29 22:13:59 +0530

    add beer validation

--- src/main/java/io/github/darkaster72/mssc_beer_service/web/model/BeerDto.java
+++ src/main/java/io/github/darkaster72/mssc_beer_service/web/model/BeerDto.java
@@ -1,10 +1,13 @@
 package io.github.darkaster72.mssc_beer_service.web.model;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
 import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Builder
@@ -13,8 +16,23 @@ public record BeerDto(
         UUID id,
         @NotBlank
         String beerName,
+        @Null
+        Integer version,
+        @Null
+        OffsetDateTime createdDate,
+        @Null
+        OffsetDateTime lastModifiedDate,
         @NotBlank
         BeerStyle beerStyle,
         @Positive
-        Long upc) {
+        @NotNull
+        Long upc,
+        @Positive
+        @NotNull
+        BigDecimal price,
+        @Positive
+        @NotNull
+        Integer quantityOnHand
+
+) {
 }
